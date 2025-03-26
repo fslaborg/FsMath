893e15f3976de52499d08ab94c33c17edace2d88
Kevin Schneider <schneider.kev@outlook.de>
2025-03-26 12:15:01 +0100

    Set up project template

---
+++ docs/index.fsx
@@ -0,0 +1,9 @@
+(***
+# FsMath
+
+Docs coming soon
+***)
+
+let a = 42
+
+(***include-value: a***)
---
+++ src/FsMath/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace GLMStats
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
